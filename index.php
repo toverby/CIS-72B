615993289c5c71604c876c2f53c1ae4e21f6e631
toverby <tyrone.overby@yahoo.com>
2014-04-22 11:00:55 -0700

    bareBones
    Upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php #index.php
+	//Tyrone Overby
+	
+	include ('header.html');
+	$page_title="cis72b";
+
+?>
+
+<!doctype html>
+	<html>
+
+		<head>
+			<meta charset="UTF-8">
+				<title>Assignment</title>
+		</head>
+
+			<body>
+
+			
+			</body>	
+	
+	</html>	
