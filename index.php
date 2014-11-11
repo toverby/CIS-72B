b788f63b0dc922d432dbda603bbda0d22bd99075
Tyrone O <contactfinite>
2014-11-11 12:18:25 -0800

    Tiddying up

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php #index.php Tyrone Overby. $page_title="cis72b"; 
-include ('header.html');	
+<?php #index.php Tyrone Overby. $page_title="cis72b";
+include ('header.html');
 ?>
 	<body class="gradient">
 
 		<div id="wrapper">
-		
 
-		
-	
+			<a href="http://codefinite.x10.mx/CIS72B/" target="_blank">root directory</a>
+
+
 		</div>
 	</body>
 
-	
 
-</html>	
+
+</html>
 
 	<footer>
 			<p class="index_footer">Designed by CodeFinite Studios</p>
